f6128d8cbf1d6c25255336b31298f009250b7905
Heitor Diniz <c.heitordiniz@gmail.com>
2018-11-20 22:01:25 -0200

    start project and settings

---
+++ app/src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <h1>Eventos</h1>
+
+export default App;
---
+++ app/src/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import Base from './styles/Base';
+
+ReactDOM.render(<Fragment>
+    <Base />
+    <App />
+</Fragment>, document.getElementById('root'));
